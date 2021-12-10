99dd9a628b44834a80ab6b5f8679f9beb45018b4
Amrita Suresh <amritasuresh2209@gmail.com>
2021-12-10 15:35:14 +0100

    Add files via upload

--- _pages/TP9/mailbox.c
+++ _pages/TP9/mailbox.c
@@ -1,20 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+#include<semaphore.h>
 
-#define MAX 10
+#define MAX 1000000
 
 int mails = 0;
+pthread_mutex_t m;
+sem_t s;
 
 void* routine() {
+    //int t1 = 0;
     for (int i = 0; i < MAX; i++) {
-        mails++;
+        sem_wait(&s);
+        pthread_mutex_lock(&m);
+        mails++; //CS
+        sem_post(&s);
+        pthread_mutex_unlock(&m);
+        //t1++;
     }
+    //printf("t1 = %d\n", t1);
     return NULL;
 }
 
 int main(int argc, char* argv[]) {
-    pthread_t p1, p2, p3, p4;
+    pthread_t p1, p2;
+    sem_init(&s, 0, 1);
+    pthread_mutex_init(&m, NULL);
     pthread_create(&p1, NULL, &routine, NULL);
     pthread_create(&p2, NULL, &routine, NULL);
 
